8c5cce765c778359de78c8a60c00a0bfc16fcd87
yash <yashahire2199@gmailcom>
2024-03-14 00:39:10 +0530

    .

--- codes/src/codes/DuplicateInArray.java
+++ codes/src/codes/DuplicateInArray.java
@@ -1,28 +1,39 @@
 package codes;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class DuplicateInArray {
-	int []number;
-	public static void dups(int[]number)
+	public static void main(String[] args)
 	{
-		
+	Scanner sc =new Scanner(System.in);
+	int size=sc.nextInt();
+	int []array=new int[size];
+	for(int i =0;i<size;i++)
+	{
+	array[i]=sc.nextInt();	
 	}
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Scanner sc=new Scanner(System.in);
-		int []size=new int[sc.nextInt()];
-		for(int i=0;i<size.length;i++)
+	System.out.println("finding duplicate element from array");
+	boolean isduplicate =false;
+	for(int i=0;i<array.length;i++)
+	{
+		for(int j=i+1;j<array.length;j++)
 		{
-			int num=sc.nextInt();
-			System.out.println(num);
+			int count=1;
+			if(array[i]==array[j])
+			{
+				count++;
+				System.out.println("duplicate element is "+array[i]);
+				System.out.println("count is "+count);
+				isduplicate=true;
+				break;
+			}
 		}
-		
-	
 	}
-
+	if(!isduplicate)
+	{
+		System.out.println("no duplicate found");
+	}
+	}
 }
-//for(int i=0;i<size.length;i++)
-//{
-//	int =sc.nextInt();
-//}
+
